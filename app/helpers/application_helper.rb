b9381fd97b8d2c92ff540a1a9cccd243c4dcc588
Jovica Zaric <jovica.zaric@gmail.com>
2011-07-04 00:08:48 +0200

    include column list in group by for postgresql

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
   end
 
   def popular_tags
-    Tag.select("DISTINCT tags.*, #{get_proper_sql_random_str} as randomizer, COUNT(tags.id) as geakets_count").joins(:geakets).group("tags.id").order("randomizer").limit(32)
+    Tag.select("DISTINCT tags.*, COUNT(tags.id) as geakets_count").joins(:geakets).group(Tag.column_names.join(", ")).order(get_proper_sql_random_str).limit(32)
 #    all(
 #      :select => "tags.*, COUNT(tags.id) as geakets_count",
 #      :joins => "INNER JOIN geakets_tags ON tags.id = geakets_tags.tag_id",
@@ -20,12 +20,13 @@ module ApplicationHelper
 
   def most_popular_tag_count
     if @most_popular_tag_count.nil?
-      most_popular_tag = Tag.first(
-        :select => "tags.*, COUNT(tags.id) as geakets_count",
-        :joins => "INNER JOIN geakets_tags ON tags.id = geakets_tags.tag_id",
-        :group => "tags.id",
-        :order => "geakets_count DESC",
-        :limit => 1)
+      most_popular_tag = Tag.select("DISTINCT tags.*, COUNT(tags.id) as geakets_count").joins(:geakets).group(Tag.column_names.join(", ")).order("geakets_count DESC").limit(1).first
+#      Tag.first(
+#        :select => "tags.*, COUNT(tags.id) as geakets_count",
+#        :joins => "INNER JOIN geakets_tags ON tags.id = geakets_tags.tag_id",
+#        :group => "tags.id",
+#        :order => "geakets_count DESC",
+#        :limit => 1)
       if most_popular_tag.nil?
         @most_popular_tag_count = 0
       else
