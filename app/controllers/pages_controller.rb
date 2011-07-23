b02412ce27226399b6ab3630d47035409ad418a9
Jovica Zaric <jovica.zaric@gmail.com>
2011-07-23 19:27:59 +0200

    vote_count bug fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @geakets = Geaket.select('geakets.*, count(votes.geaket_id) as vote_count').joins('LEFT JOIN votes ON geakets.id = votes.geaket_id').group(Geaket.column_names.map{|column| "#{Geaket.table_name}.#{column}"}.join(", ")).order('vote_count DESC').paginate(:page => params[:page])
+    @geakets = Geaket.select('geakets.*, count(votes.geaket_id)').joins('LEFT JOIN votes ON geakets.id = votes.geaket_id').group(Geaket.column_names.map{|column| "#{Geaket.table_name}.#{column}"}.join(", ")).order('count(votes.geaket_id) DESC').paginate(:page => params[:page])
   end
 
   def about
